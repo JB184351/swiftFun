fb928339fd47a17c8186c20c75c034db2a8a5f02
JB184351 <justinbengtson@icloud.com>
2018-12-31 19:04:05 -0800

    Changed app to Tip Calculator

--- swiftFun/swiftFun/ViewController.swift
+++ swiftFun/swiftFun/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
 
     var count = 0
     
+    // Represents the total the person will have to pay
     @IBOutlet var label: UILabel!
+
+    // Total Bill before Tip
+    @IBOutlet var total: UITextField!
+    
+    // Gratitude %
+    @IBOutlet var tipPercentage: UITextField!
     
     
     override func viewDidLoad() {
@@ -20,14 +27,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
     @IBAction func buttonPressed(_ sender: Any) {
+        /*
         count += 1
         label.text = String(count)
         
         if count >= 10 {
             view.backgroundColor = UIColor.green
         }
+        */
+        
+        var amountTotal = Double(total.text!)!
+        var tipAmount = Double(tipPercentage.text!)! / 100.0
+        
+        var tip = amountTotal * tipAmount
+        
+        
+        // Printing the tip total
+        if tip > 50.0 {
+            label.text = "Your Tip is $\(tip)!"
+        }
+        
+        else {
+            label.text = "Tip: $\(tip)"
+        }
     }
+ 
     
 
 
